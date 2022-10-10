677450d66d95e23127b69265a617d4d016c9c162
George <47188474+GCMoony@users.noreply.github.com>
2022-10-10 16:03:09 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "*.{html,js}",
-    "./.src/*.{css}"
-           ],
+  content: ["*.{html,js}"],
   theme: {
     extend: {},
   },
